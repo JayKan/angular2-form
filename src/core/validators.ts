065b534227fc6aedac8eaf260f8a24a9d5a22f28
Jay Kan <leicasper@gmail.com>
2016-03-29 15:50:06 -0700

    feat(validators): add custom credit card validator

--- src/core/validators.ts
+++ src/core/validators.ts
@@ -1,10 +1,19 @@
-
 export class CustomValidators {
+
   static zipCodeValidator(control): any {
-    let valid: boolean = /^\d{5}$/.test(control.value);
+    let valid: boolean = /^\d{5}$/.test(control.value); // 5 digits numbers ONLY
     if (valid) {
       return null;
     }
-    return {'invalidZip': true}
+    return {'invalidZip': true};
+  }
+
+  static creditCardValidator(control): any {
+    // Visa, MasterCard, American Express, Diners Club, Discover, JCB
+    let valid: boolean = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(control.value);
+    if (valid) {
+      return null;
+    }
+    return {'invalidCreditCard': true};
   }
 }
