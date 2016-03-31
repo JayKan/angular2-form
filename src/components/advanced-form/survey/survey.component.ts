decc114a42503875560ef4b2458c9cbb97501c1c
Jay Kan <leicasper@gmail.com>
2016-03-31 15:05:31 -0700

    refactor(advanced-form@survey): templateURl instead of inline template

--- src/components/advanced-form/survey/survey.component.ts
+++ src/components/advanced-form/survey/survey.component.ts
@@ -6,39 +6,7 @@ import { ControlMessages } from '../../../core/control-messages/control-messages
 @Component({
   selector: 'survey',
   directives: [ControlMessages],
-  template: `
-  <div>
-    <p>Survey using Dynamic Form:</p>
-    <form (ngSubmit)="onSubmit()" [ngFormModel]="form" #f="ngForm">     
-      <div *ngFor="#question of model.questions" class="form-group">      
-        <label [attr.for]="question.key">{{ question.text }}</label>
-        <div [ngSwitch]="question.controlType">
-          <div *ngSwitchWhen="'textbox'">
-            <input class="form-control"
-                   [type]="question.type" 
-                   [id]="question.key"
-                   ngControl="{{ question.key }}">
-          </div>          
-          <div *ngSwitchWhen="'dropdown'">
-            <select class="form-control" id="{{ question.key }}" ngControl="{{ question.key }}">
-              <option *ngFor="#o of question.options" [value]="o.key">{{ o.value }}</option>
-            </select>
-          </div>
-        </div>        
-        <control-messages [control]="question.key"></control-messages>
-      </div>      
-      <button type="submit" class="btn btn-default" [disabled]="!form.valid">Submit</button>
-    </form>
-    <hr>
-    
-    <div [hidden]="!submitted">
-      <div *ngIf="formData">
-        <strong>You have submitted the following: </strong>
-        <pre>{{ formData }}</pre>
-      </div>
-    </div>
-  </div>
-  `,
+  templateUrl: 'components/advanced-form/survey/survey.component.html'
 })
 
 export class Survey implements OnInit {
